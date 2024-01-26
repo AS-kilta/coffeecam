24a32db6b14669372ca0dc1a4269dbf3e99984e6
Ileska <ileska@tutanota.com>
2024-01-26 19:28:13 +0200

    Wrap all inside function

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ import cv2
 
 from decouple import config
 
-TAPO_USERNAME = config("TAPO_USERNAME")
-TAPO_PASSWORD = config("TAPO_PASSWORD")
+def gitImage():
+	TAPO_USERNAME = config("TAPO_USERNAME")
+	TAPO_PASSWORD = config("TAPO_PASSWORD")
 
-HOST = "192.168.1.45"  # The server's hostname or IP address
-PORT = 554  # The port used by the server
+	HOST = "192.168.1.45" # The server's hostname or IP address
+	PORT = 554 # The port used by the server
 
-url = f"rtsp://{TAPO_USERNAME}:{TAPO_PASSWORD}@{HOST}:{PORT}/stream1"
-savePath = "pic1.jpeg"
+	url = f"rtsp://{TAPO_USERNAME}:{TAPO_PASSWORD}@{HOST}:{PORT}/stream1"
+	savePath = "pic1.jpeg"
 
-try:
-    stream = cv2.VideoCapture(url)
-except Exception as g:
-    print("no stream")
+	try:
+		stream = cv2.VideoCapture(url)
+	except Exception as g:
+		print("no stream")
 
-success, image = stream.read()
+	success, image = stream.read()
 
-cv2.imwrite(savePath, image)
+	cv2.imwrite(savePath, image)
+
+if __name__ == "__main__":
+
